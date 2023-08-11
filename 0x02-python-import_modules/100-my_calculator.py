727cc2fd0199e0a7b26a6ff707d06e9a2eb22d02
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-11 17:05:06 +0000

    Added 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    argv_len = len(sys.argv)
+    if(argv_len != 4):
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+
+    if op == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{} + {} = {}".format(a, b, sub(a, b)))
+    elif op == "*":
+        print("{} + {} = {}".format(a, b, mul(a, b)))
+    elif op == "/":
+        print("{} + {} = {}".format(a, b, div(a, b)))
+    else:
+        print(f"Unkown operator. Available operators: +, -, * and /")
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -5,4 +5,5 @@ File 1-calculation.py - imports and uses calculator functions from calculator_1
 File 2-args.py - prints number of and the list of its arguments  
 File 3-infinite_add.py - prints the result of addition of all arguments  
 File 4-hidden_discovery.py - prints all names defined by the compiled comdule hidden_4.pcy  
-File 5-variable_load.py - imports and prints a variable
+File 5-variable_load.py - imports and prints a variable  
+File 100-my_calculator.py - imports module to handle basic operations

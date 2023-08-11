cf55680fab266948bbe3c89ff835e25349b96e1a
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-11 17:14:41 +0000

    Hot fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
     if op == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == "-":
-        print("{} + {} = {}".format(a, b, sub(a, b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == "*":
-        print("{} + {} = {}".format(a, b, mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif op == "/":
-        print("{} + {} = {}".format(a, b, div(a, b)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print(f"Unkown operator. Available operators: +, -, * and /")
         sys.exit(1)

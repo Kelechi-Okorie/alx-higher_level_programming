496af2a5de37c319c10fdb80d176eb3d344b81c3
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-12 18:16:54 +0000

    Hot fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+
+    if (len(my_list) == 0):
+        return None
+
     list_len = len(my_list) - 1
 
     while list_len >= 0:

97a8469331488cf172fa977aab43dd881fa22e4b
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-18 10:27:36 +0000

    Hot fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    if matrix:
-        return list(map(lambda r: [r[0] * r[0], r[1] * r[1], r[2] * r[2]],
-                    matrix))
-
-    return []
+    return list(map(lambda r: [r[0] * r[0], r[1] * r[1], r[2] * r[2]], matrix))

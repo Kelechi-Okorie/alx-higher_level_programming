3c7808ac493974a76e44b9251206417320c5f681
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-28 11:43:23 +0000

    Hot fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,15 +3,14 @@
 def safe_print_list(my_list=[], x=0):
     n = 0
 
-    if my_list:
-        for i in my_list:
-            try:
-                print(i, end="")
-                n += 1
-                if n == x:
-                    break
-            except IndexError:
+    for i in my_list:
+        try:
+            print(i, end="")
+            n += 1
+            if n == x:
                 break
+        except:
+            pass
 
-    print("")
+    print()
     return n

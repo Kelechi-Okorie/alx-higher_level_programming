cd3289f0c4266cc4342a64db597278b9ae015ebc
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-28 11:19:49 +0000

    Added 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+
+    for i in my_list:
+        try:
+            print(i, end="")
+            n += 1
+            if n == x:
+                break
+        except IndexError:
+            break
+
+    print("")
+    return n
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x05-python-exceptions
+
+File 0-safe_print_list.py - prints x elements of a list safely

f032db50a2863acc2b989b0b162bb3c1242394d4
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-29 20:16:05 +0000

    Added 4-style.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""The square class
+
+The square class with method for
+calculating areas
+"""
+
+
+class Square:
+    """A square class with size attribute and area method"""
+
+    def __init__(self, size=0):
+        """The init method"""
+        self.size = size
+
+    @property
+    def size(self):
+        """The getter."""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """The setter."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """The area of the square"""
+        return self.__size * self.__size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -3,4 +3,5 @@
 File 0-square.py - An empty class Square that defines a square  
 File 1-square.py - A class that defines a square  
 File 2-square.py - A class that defines a square with optional size  
-File 3-square.py - A class that defines a square with an area method
+File 3-square.py - A class that defines a square with an area method  
+File 4-square.py - A class that defines a square with  area, and setter and getter method for size

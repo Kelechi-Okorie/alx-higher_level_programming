2cdc560e80e62be0109151050a0b5f004ce02ef6
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-29 17:30:39 +0000

    Added 0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    """This is an empty class
+
+    This class does not do anythin
+    """
+
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x06-python-classes
+
+File 0-square.py - An empty class Square that defines a square

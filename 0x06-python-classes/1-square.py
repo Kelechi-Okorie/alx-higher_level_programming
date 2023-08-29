bc851a452f01f1155e5f25919a70f689f38d67d1
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-08-29 17:48:46 +0000

    Added square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """A square class with a size"""
+
+    def __init__(self, size=0):
+        """The init method for the class
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,3 +1,4 @@
 # Readme file for 0x06-python-classes
 
-File 0-square.py - An empty class Square that defines a square
+File 0-square.py - An empty class Square that defines a square  
+File 1-square.py - A class that defines a square

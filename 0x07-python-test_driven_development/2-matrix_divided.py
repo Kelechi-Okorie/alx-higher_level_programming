ffadf368826ea7f04538c014f13504d0e9d37c3c
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-08 11:11:52 +0000

    Added 2-matrix_divided.py

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module matrix_divided
+divides all elements of a matrix
+
+"""
+def matrix_divided(matrix, div):
+    """Divides all elements of matrix by div
+    rounded to 2 decimal places,
+    where div is either an int or a float
+    and matrix is a list of list
+
+    """
+    if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                "of integers/floats")
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for r in matrix:
+        if len(r) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                    "of i ntegers/floats")
+
+        for c in r:
+            if type(c) is not int and type(c) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
+
+    length_rows = []
+    for r in matrix:
+        length_rows.append(len(r))
+    if not all(length == length_rows[0] for length in length_rows):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    return [[round(c / div, 2) for c in r] for r in matrix]
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,3 +1,4 @@
 # Readme file for 0x07-python-test_driven_development
 
-Files 0-add_integer.py, tests/0-add_integer.txt - A function that adds 2 integers with test cases
+Files 0-add_integer.py, tests/0-add_integer.txt - A function that adds 2 integers with test caseis  
+Files 2-matrix_divided.py, tests/2-matrix_divided.txt - A function that divides all elements of a matrix

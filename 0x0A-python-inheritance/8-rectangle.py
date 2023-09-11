6b55a820835e411dcd03384634f4a8348712965b
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-11 17:43:09 +0000

    Bug fix on 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,12 +8,22 @@ The BaseGeometry module
 
 
 class Rectangle(BaseGeometry):
-    """Base Geometry class"""
+    """Rectangle class
+    inherits from BaseGeometry
+    private attributes:
+        - width
+        - height
+    """
 
     def __init__(self, width, height):
-        """Initializes a rectangle object"""
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
+        """Initializes a rectangle object
+
+        Args:
+            - width: with of the rectangle
+            - height: height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
         self.__width = width
         self.__height = height

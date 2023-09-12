e2b5f0dd91de64223b71115f1bc4af09ad26253a
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-12 11:15:35 +0000

    Hot fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,11 @@ Reads a file
 
 
 def read_file(filename=""):
+    """reads a file and prints its contents
+
+    Args:
+        - filename: name of the file to read
+    """
     with open(filename, encoding="utf-8") as f:
         read_file = f.read()
         print(read_file, end="")

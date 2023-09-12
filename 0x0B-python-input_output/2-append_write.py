24225c8cf2e731128bb414164f922d624f649e0c
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-12 11:33:46 +0000

    Hot fix on 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,11 @@ Appends to the end of a file
 
 
 def append_write(filename="", text=""):
+    """appends content to a file
+
+    Args:
+        - filename: the file to append to
+        - text: the text to append to the file
+    """
     with open(filename, "a") as f:
         return f.write(text)

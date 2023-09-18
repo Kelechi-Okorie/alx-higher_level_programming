d68e0d14b3073ea607f8aa5d9907d575b7f3b8e7
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-18 21:39:10 +0000

    Added to_json_string static method and unit tests for it

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ This is the base of all other classes in the project
 """
 
 
+import json
+
+
 class Base:
     """The Base class
     """
@@ -25,3 +28,22 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns a JSON representation of list_dictionaries
+
+        Args
+            - list_dictionaries: list of dictionaries
+
+        Returns: JSON representation of the list
+        """
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        if (type(list_dictionaries) is not list or
+                not all(type(x) is dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -34,3 +34,73 @@ class TestBase(unittest.TestCase):
         base = Base()
         self.assertTrue(isinstance(base, Base))
         self.assertEqual(type(base), Base)
+
+    def test_15_to_json_string(self):
+        """Test to_json static method with valid arguments"""
+
+        my_dict = {"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}
+        my_json = Base.to_json_string([my_dict])
+        self.assertTrue(isinstance(my_dict, dict))
+        self.assertTrue(isinstance(my_json, str))
+        self.assertCountEqual(
+                my_json, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+        json_dict_1 = Base.to_json_string([])
+        self.assertEqual(json_dict_1, "[]")
+        json_dict_2 = Base.to_json_string(None)
+        self.assertEqual(json_dict_1, "[]")
+
+    def test_15_exception(self):
+        """Test to_json_string static method with wrong arguments"""
+
+        msg = "list_dictionaries must be a list of dictionaries"
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string(2)
+        self.assertEqual(str(x.exception), msg)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string("alx")
+        self.assertEqual(str(x.exception), msg)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string([1, 2, 3, 4])
+        self.assertEqual(str(x.exception), msg)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string(["alx", "school"])
+        self.assertEqual(str(x.exception), msg)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string(2.3)
+        self.assertEqual(str(x.exception), msg)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string({1: 'alx', 2: 'school'})
+        self.assertEqual(str(x.exception), msg)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string((1, 2))
+        self.assertEqual(str(x.exception), msg)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string(True)
+        self.assertEqual(str(x.exception), msg)
+
+    def test_15_exception(self):
+        """Testing Base class to_json_string method with wrong arguments"""
+
+        msg1 = ("to_json_string() missing 1 required positional argument: " +
+                "'list_dictionaries'")
+
+        msg2 = "to_json_string() takes 1 positional argument but 2 were given"
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string()
+        self.assertEqual(str(x.exception), msg1)
+
+        with self.assertRaises(TypeError) as x:
+            Base.to_json_string([{1, 2}], [{3, 4}])
+        self.assertEqual(str(x.exception), msg2)
+
+if __name__ == "__main__":
+    unittest.main()

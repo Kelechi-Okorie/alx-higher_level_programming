7389086a73e1c350f5c6a1e2c7b1578a74e996fc
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-30 22:14:31 +0000

    Added 6-rectangle.py

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+""" Rectangle class bases on 0-rectangle.py """
+
+
+class Rectangle:
+    """ Rectangle class bases on 0-rectangle """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """ Constructor for rectangle class
+
+        Args:
+            - wdith: with of the rectangle instance
+            - height: height of the rectangle instance
+
+        Returns: Nothing
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """ returns width of the instance
+
+        Returns: width of the rectangle
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ sets the width of the instance
+
+        Args
+            - width: the new width
+
+        Returns: nothing
+        """
+
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+
+        if width < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = width
+
+    @property
+    def height(self):
+        """ returns the height of the rectangle
+
+        Return: height of the rectangle
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ sets the height of the rectangle
+
+        Args
+            - height: the new height to set
+
+        Returns: nothing
+        """
+
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+
+        if height < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = height
+
+    def area(self):
+        """ Calculates and returns the area of the rectangle """
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Calculates and returns the perimeter of the rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        """ returns the string representation of the rectangle """
+
+        string = ""
+
+        if self.__width == 0 or self.__height == 0:
+            return string
+
+        for h in range(0, self.__height):
+            for w in range(0, self.__width):
+                string = string + "#"
+            string = string + "\n"
+
+        return string[:-1]
+
+    def __repr__(self):
+        """ returns a string representation of the rectangle to be
+        able to recreate a new instance by using eval()
+        """
+
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """ The destructor for Rectangle class """
+
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -5,4 +5,5 @@ File 1-rectangle.py - class Rectangle that defines a rectangle based on 0-rectan
 File 2-rectangle.py - class Rectangle that enhances 1-rectangle.py by adding area and perimeter instance methods   
 File 3-rectangle.py - class Rectangle that enhances 2-rectangle by adding __str__ method  
 File 4-rectangle.py - class Rectangle that enhances 3-rectangle by adding __repr__ method  
-File 5-rectangle.py - class Rectangle that enhances 4-rectangle by printing a message when an instance of Rectangle is being deleted
+File 5-rectangle.py - class Rectangle that enhances 4-rectangle by printing a message when an instance of Rectangle is being deleted  
+File 6-rectangle.py - class Rectangle that enhances 5-rectangle by adding public class attribute number_of_instances

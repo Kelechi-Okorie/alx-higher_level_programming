c038ccaff6224682343eb895af887c7c0f0358d0
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-09-30 20:52:40 +0000

    Added an empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" empty class Rectangle module """
+
+
+class Rectangle:
+    """ Empty rectangle class """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# Readme file for 0x08-python-more_classes
+
+File 0-rectangle.py - an empty class Rectangle that defines a rectangle

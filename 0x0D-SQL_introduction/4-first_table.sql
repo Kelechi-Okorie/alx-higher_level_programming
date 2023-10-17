cf35f311547c9f1b482d1e1f7109113554d38625
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-17 19:37:32 +0100

    Minor modifications

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creates a table in the current db
 -- name passed as argument to mysql command
-CREATE TABLE IF NOT EXISTS first_table(it INT, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256))

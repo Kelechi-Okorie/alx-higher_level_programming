5065da505e7f82f6478397985b03c3762bf60115
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-18 19:05:29 +0100

    Minor bug fix on task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- should not fail if db or user exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create the new user
-CREATE USER IF NOT EXISTS user_0d_2 identified by 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2 identified by 'User_0d_2_pwd';
 -- grant only select privilege to the user on hbtn_0d_2 db
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

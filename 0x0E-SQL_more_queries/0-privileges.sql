523b22833c85eea87e7c9f4506bef976395f531f
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-18 17:15:12 +0100

    Hot fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,4 +2,4 @@
 -- users are user_0d_1 annd user_0d_2
 SHOW GRANTS FOR user_0d_1@localhost
 -- lists all granted privileges for the second user
-SHOW GRANTS for user_0d_2@localhost
+SHOW GRANTS FOR user_0d_2@localhost

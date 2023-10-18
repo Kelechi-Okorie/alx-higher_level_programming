ffc8e351a9cbbbedd37ce37b4cf73b9084686c6b
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-18 22:39:00 +0100

    Hot fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 -- table schema (id INT UNIQUE AUTOGENERATE NOT NULL PRIMARY KEY, name VARCHAR(256))
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create the table
-Use hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL, PRIMARY KEY(id));

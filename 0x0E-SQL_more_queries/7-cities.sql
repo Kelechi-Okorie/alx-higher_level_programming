7542e7ed193d2875d55b517bb373852da49d9bde
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-18 19:51:13 +0100

    Bug fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- The script should not fail if db or table exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- cities description (id, state_id, name)
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES state(id))
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES states(id))

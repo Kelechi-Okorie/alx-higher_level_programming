26120eeeb613d19fcdc29e8a5ddf82b835d6f927
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-18 20:10:32 +0100

    Hot fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all cities of california
 -- db name passed as argument
-SELECT id, name FROM cities WHERE stat_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = California) ORDER BY id ASC

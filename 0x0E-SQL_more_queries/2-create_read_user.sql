dd7dd58167021a17ac9709651ba1b8e027a6c062
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-18 18:56:38 +0100

    Hot fix on task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create the new user
 CREATE USER IF NOT EXISTS user_0d_2 identified by 'user_0d_2_pwd';
 -- grant only select privilege to the user on hbtn_0d_2 db
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost ;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

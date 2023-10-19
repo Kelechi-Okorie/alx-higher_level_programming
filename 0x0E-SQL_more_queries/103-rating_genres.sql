08da0a64bf86ef0a72827f5116828a34f1bf14aa
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-19 22:28:41 +0100

    Buf fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- lists all genres in the db
 -- db name will be passed as an argument
-SELECT tv_genres.name, SUM(tv_ratings.rate) AS 'rating'
+SELECT name, SUM(tv_ratings.rate) AS 'rating'
 FROM tv_genres
 LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT OUTER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id

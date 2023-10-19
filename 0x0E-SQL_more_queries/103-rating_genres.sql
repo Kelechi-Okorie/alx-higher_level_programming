9d934534050cd65fbb2164f80a64d232ba2e93ad
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-10-19 22:29:47 +0100

    Hot fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,7 @@
 -- db name will be passed as an argument
 SELECT name, SUM(tv_ratings.rate) AS 'rating'
 FROM tv_genres
-LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT OUTER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY name
 ORDER BY name ASC;

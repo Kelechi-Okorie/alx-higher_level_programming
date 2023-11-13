24a96068372ffa30e380ec07c3ea00bd4c057e7e
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-11-13 17:31:38 +0100

    Minor fix on task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const num = Number.parseInt(process.argv[2]);
-if (isNaN(num)) {
+if (isNaN(num) || process.arg[2] === undefined) {
   console.log('Not a number');
 } else {
   console.log(num);

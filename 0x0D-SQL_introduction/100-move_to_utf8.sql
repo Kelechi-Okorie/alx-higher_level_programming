6bde5130ce4071fa2da9fdb793458c044e6ea535
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-11-13 14:21:25 +0100

    Added 0x12-javascript_warm_up project

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- converts htbn_0c_0 db to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 -- both the db and first table
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- convert first_table to utf8
+-- convert first_table to utf8mb4
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- convert name column to utf8
+-- convert name column to utf8mb4
 ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

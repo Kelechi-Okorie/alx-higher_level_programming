9ee1a0d160e188fa850e943c1f2ce61357fad8bb
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-11-13 18:11:33 +0100

    Bug fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
   const list = process.argv.slice(2).map(num => Number.parseInt(num));
   const sorted = list.sort((a, b) => b - a);

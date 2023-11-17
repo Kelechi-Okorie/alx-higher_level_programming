f155435c984919d821865656d7198ca0ac7ad9ee
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-11-17 17:46:54 +0100

    Added task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""displays states according to the user input
+while protecting against sql injection"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    host = 'localhost'
+    port = 3306
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(host=host, port=port, user=username,
+                         passwd=password, db=dbname)
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name=%s\
+        ORDER by states.id ASC".format(state)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -2,4 +2,5 @@
 
 File 0-select_states.py - a script that lists all states fromt he db hbtn_0e_0_usa  
 File 1-filter_states.py - lists all scripts with name starting with N from the db hbtn_0e_0_usa  
-File 2-my_filter_states.py - takes in an arument and displays all values in the states table of the hbtn_0e_0_usa where name matches the argument
+File 2-my_filter_states.py - takes in an arument and displays all values in the states table of the hbtn_0e_0_usa where name matches the argument  
+File 3-my_safe_filter_states.py - same as task 2 but add protection against sql injection  

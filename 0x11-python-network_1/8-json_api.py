97fa692cd140be4e6aebbf8ea64a9b89524ab73c
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-12-02 20:45:40 +0100

    Added task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Takes in a letter and sends a post request
+with the letter as a parameter"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    data = {"q": q}
+    res = requests.post(url, data)
+    try:
+        result = res.json()
+        if result == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get('id'), res.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -7,3 +7,4 @@ File 4-hbtn_status.py - fetches a url using requests package
 File 5-hbtn_header.py - sends a request to a url, using requests package and displays the value of the X-Request-Id variable found in the response header  
 File 6-post_email.py - sends a POST request to a url with email as a parameter using requests package 
 File 7-error_code.py - sends request to a url and displays error message  
+File 8-json_api.py - takes in a letter and sends a post request with the letter as a parameter  
